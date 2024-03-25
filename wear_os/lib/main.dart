8c5988038c5dfcc57bfc9f31669d2454cfdc4cb3
Tiago Cruz <tiagofcruz78@ua.pt>
2024-03-25 17:24:43 +0000

    water data being sent

--- wear_os/lib/main.dart
+++ wear_os/lib/main.dart
@@ -1,9 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:workout/workout.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
 import 'package:weather/weather.dart';
-import 'WaterIntakePage.dart';
 import 'WorkoutDetailsPage.dart';
 
 void main() => runApp(const MyApp());
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
           shadowColor: Colors.black,
           elevation: 5,
         ),
-        textTheme: TextTheme(
+        textTheme: const TextTheme(
           bodyText2: TextStyle(color: Colors.white),
         ),
       ),
@@ -135,7 +136,8 @@ class _SensorStatsPageState extends State<SensorStatsPage> {
 
   void publishSensorData() {
     final sensorData =
-        'Heart Rate: $heartRate, Calories: $calories, Steps: $steps, Distance: $distance, Speed: $speed';
+        'Heart Rate: $heartRate, Calories: $calories, Steps: $steps, Distance: $distance, Speed: $speed, Water Intake: $waterIntake';
+
     final builder = MqttClientPayloadBuilder();
     builder.addString(sensorData);
 
@@ -144,26 +146,41 @@ class _SensorStatsPageState extends State<SensorStatsPage> {
 
   @override
   Widget build(BuildContext context) {
+    // Use MediaQuery to get the screen size
+    final screenSize = MediaQuery.of(context).size;
+    final pageHeight = screenSize.height;
+    final pageWidth = screenSize.width;
+
+    // Calculate the size of the content to ensure it's visible in a round display
+    // This is a basic approach; you might need to adjust it based on your actual content
+    final contentSize = min(pageWidth, pageHeight);
+
     return Scaffold(
-      body: GridView.count(
-        crossAxisCount: 2,
+      body: PageView(
+        controller: PageController(
+            viewportFraction: 1.0), // Ensure each page takes full screen
+        scrollDirection: Axis.vertical,
         children: [
-          _statTile('Heart Rate', '$heartRate bpm', Icons.favorite, Colors.red),
-          _statTile(
-              'Weather',
-              currentWeather != null
-                  ? "${currentWeather!.weatherDescription}, ${currentWeather!.temperature!.celsius!.toStringAsFixed(1)}°C"
-                  : "Loading...",
-              Icons.wb_sunny,
-              Colors.orange),
-          _waterIntakeTile(),
-          _workoutNavigatorTile(context),
+          Center(
+              child: _statTile('Heart Rate', '$heartRate bpm', Icons.favorite,
+                  Colors.red, contentSize)),
+          Center(
+              child: _statTile(
+                  'Weather',
+                  currentWeather != null
+                      ? "${currentWeather!.weatherDescription}, ${currentWeather!.temperature!.celsius!.toStringAsFixed(1)}°C"
+                      : "Loading...",
+                  Icons.wb_sunny,
+                  Colors.orange,
+                  contentSize)),
+          Center(child: _waterIntakeTile(contentSize * 0.95)),
+          Center(child: _workoutNavigatorTile(context, contentSize)),
         ],
       ),
     );
   }
 
-  Widget _workoutNavigatorTile(BuildContext context) {
+  Widget _workoutNavigatorTile(BuildContext context, double size) {
     return GestureDetector(
       onTap: () => Navigator.push(
           context,
@@ -173,16 +190,35 @@ class _SensorStatsPageState extends State<SensorStatsPage> {
                   steps: steps,
                   distance: distance,
                   speed: speed))),
-      child: Card(
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          color: Colors.grey[800],
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black,
+              spreadRadius: 2,
+              blurRadius: 4,
+              offset: Offset(0, 2),
+            )
+          ],
+        ),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(Icons.fitness_center, size: 20, color: Colors.green),
-              SizedBox(height: 6),
-              Text('Workout',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.green)),
+              Icon(Icons.fitness_center, size: size * 0.3, color: Colors.green),
+              SizedBox(height: size * 0.05),
+              Text(
+                'Workout',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green,
+                  fontSize: size * 0.1,
+                ),
+              ),
             ],
           ),
         ),
@@ -190,49 +226,133 @@ class _SensorStatsPageState extends State<SensorStatsPage> {
     );
   }
 
-  Widget _statTile(String title, String value, IconData icon, Color color) {
-    return Card(
+  Widget _statTile(
+      String title, String value, IconData icon, Color color, double size) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        color: Colors.grey[800],
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black,
+            spreadRadius: 2,
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          )
+        ],
+      ),
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(icon, size: 20, color: color),
-            SizedBox(height: 6),
-            Text(title,
-                style: TextStyle(fontWeight: FontWeight.bold, color: color)),
-            Text(value, style: TextStyle(fontSize: 12)),
+            Icon(icon, size: size * 0.3, color: color),
+            SizedBox(height: size * 0.05),
+            Text(
+              title,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: color,
+                fontSize: size * 0.1,
+              ),
+            ),
+            Text(
+              value,
+              style: TextStyle(
+                fontSize: size * 0.08,
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget _waterIntakeTile() {
-    return GestureDetector(
-      onTap: () => Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => WaterIntakePage(
-                  initialWaterIntake: waterIntake,
-                  onWaterIntakeUpdated: (int updatedValue) {
-                    setState(() {
-                      waterIntake = updatedValue;
-                    });
-                  }))),
-      child: Card(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.local_drink, size: 20, color: Colors.blue),
-              SizedBox(height: 6),
-              Text('Water',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.blue)),
-              Text('$waterIntake', style: TextStyle(fontSize: 18)),
-            ],
+  Widget _waterIntakeTile(double size) {
+    double progressValue = waterIntake / 8.0; // Assuming 8 is the target value
+
+    return Container(
+      width: size,
+      height: size,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          // Circular Progress Indicator
+          SizedBox(
+            width: size,
+            height: size,
+            child: CircularProgressIndicator(
+              value: progressValue, // Current progress
+              strokeWidth: 10, // Width of the progress indicator
+              backgroundColor:
+                  Colors.grey, // Background color of the progress bar
+              valueColor: AlwaysStoppedAnimation<Color>(
+                  Colors.blue), // Color of the progress bar
+            ),
           ),
-        ),
+          // Water Intake Content
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[800],
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  spreadRadius: 2,
+                  blurRadius: 4,
+                  offset: Offset(0, 2),
+                ),
+              ],
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.local_drink, size: size * 0.3, color: Colors.blue),
+                  SizedBox(height: size * 0.05),
+                  Text(
+                    'Water',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                      fontSize: size * 0.1,
+                    ),
+                  ),
+                  Text(
+                    '$waterIntake',
+                    style: TextStyle(
+                      fontSize: size * 0.08,
+                    ),
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.remove, color: Colors.white),
+                        onPressed: () {
+                          setState(() {
+                            if (waterIntake > 0) {
+                              waterIntake--;
+                            }
+                          });
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.add, color: Colors.white),
+                        onPressed: () {
+                          setState(() {
+                            waterIntake++;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
